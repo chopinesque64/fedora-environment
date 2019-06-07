a3fddf32259de46bc907d60da486eb472b755ab8
Nathanael Gentry <ngentry1@liberty.edu>
2019-06-07 12:05:35 -0400

    Group packages

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+# Package Installation
+sudo dnf install vim \
+guake \
+docker \
+docker-compose \
+zsh \
+emacs \
+firefox
+
 # Cinnamon
 dconf load /org/cinnamon/ < configs/cinnamon-dconf
 
 # Vim
-sudo dnf install vim
 ln -sf "$PWD/configs/vimrc" ~/.vimrc
+mkdir ~/.vim/colors
+wget
+https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+~/.vim/colors/
 
-# Git Configuration
+# Git
 git config --global credential.helper "cache --timeout=3600"
 git config --global push.default simple
 git config --global user.email ngentry1@liberty.edu
@@ -17,26 +29,21 @@ git config --global commit.gpgsign true
 sudo ln -sf "$PWD/configs/post-commit" /usr/share/git-core/templates/hooks/
 
 # Guake
-sudo dnf install guake
 sudo ln -sf /usr/share/applications/guake.desktop ~/.config/autostart/
 gconftool-2 --load configs/apps-guake.xml
 gconftool-2 --load configs/schemas-apps-guake.xml
 
 # Docker
-sudo dnf install docker docker-compose
 sudo systemctl enable docker
 
 # Zsh
-sudo dnf install zsh
 sudo ln -sf "$PWD/configs/zshrc.system" /etc/zshrc
 sudo ln -sf "$PWD/configs/zshrc.local" ~/.zshrc
 chsh --shell $(which zsh) $USER
 
 # Emacs
-sudo dnf install emacs
 mkdir ~/.emacs.d
 ln -sf "$PWD/configs/init.el" ~/.emacs.d/
 
 # Firefox
-sudo dnf install firefox
 ln -sf "$PWD/configs/user.js" $(find ~/.mozilla/firefox -type d -name "*.default" -print -quit)
