7f0f0836fadb519e7de1a6b086895330e963b7b3
Nathanael Gentry <ngentry1@liberty.edu>
2019-06-06 23:17:09 -0400

    Add Git configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Git Configuration
+git config --global credential.helper "cache --timeout=3600"
+git config --global push.default simple
+git config --global user.email ngentry1@liberty.edu
+git config --global user.name "Nathanael Gentry"
+git config --global user.signingkey 4C7C95EC9EF26D41 
+git config --global commit.gpgsign true
+
 # Guake
 sudo dnf install guake
 sudo ln -sf /usr/share/applications/guake.desktop ~/.config/autostart/
