7f1ab9df2047db0dc21fe1cde9dd297396f25ca4
Nathanael Gentry <ngentry1@liberty.edu>
2019-07-22 02:23:58 -0400

    remove Write

--- scripts/setup-write.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p ~/bin
-wget -qO- http://www.styluslabs.com/write/write209.tar.gz | tar -C ~/bin -xvz
-wget https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Icon-notepad.svg/256px-Icon-notepad.svg.png -O ~/bin/Write/write.png
-sudo ln -sf $PWD/../configs/Write.desktop /usr/share/applications 
