2038424850da242c90fc76d540f5152bb6f48783
Nathanael Gentry <ngentry1@liberty.edu>
2019-07-22 18:00:22 -0400

    emacs: add org archive

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,10 @@
 ;; Configure MELPA
 (require 'package)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(dolist (repo '(("melpa" . "https://melpa.org/packages/")
+                ("org" . "https://orgmode.org/elpa/")))
+  (add-to-list 'package-archives repo t))
+
 (package-initialize)
 
 ;; Ensure that use-package is installed
